1f013f3ec8d927cf455ff29ed80c833b1ab3c0c7
denis_levchenko <tranzi90@gmail.com>
2022-06-04 03:31:42 +0300

    clean initial files and setup App component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="post">
+      <div><strong>Title:</strong> Post about JavaScript</div>
+      <div><strong>Description:</strong> JavaScript is universal programming language</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      ++this.likes
+    },
+    addDislike() {
+      ++this.dislikes
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
