f079111c1734de4f826e2fa279800df90d07b15d
denis_levchenko <tranzi90@gmail.com>
2022-06-05 03:03:22 +0300

    create UI elements and styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-    <div class="post">
-      <div><strong>Title:</strong> Post about JavaScript</div>
-      <div><strong>Description:</strong> JavaScript is universal programming language</div>
+  <div class="app">
+    <form>
+      <h4>Create post</h4>
+      <input
+          type="text"
+          placeholder="Title"
+          v-bind:value="title"
+          @input="title = $event.target.value"
+      >
+      <input
+          type="text"
+          placeholder="Description"
+          v-bind:value="body"
+          @input="body = $event.target.value"
+      >
+      <button @click="createPost">Post</button>
+    </form>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+        <div><strong>Title:</strong> {{ post.title }} </div>
+        <div><strong>Description:</strong> {{ post.body }} </div>
     </div>
   </div>
 </template>
@@ -11,21 +27,53 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Post description'},
+        {id: 2, title: 'JavaScript 2', body: 'Post description 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Post description 3'},
+      ],
+      title: '',
+      body: ''
     }
   },
   methods: {
-    addLike() {
-      ++this.likes
-    },
-    addDislike() {
-      ++this.dislikes
+    createPost() {
+
     }
   }
 }
 </script>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+button {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
